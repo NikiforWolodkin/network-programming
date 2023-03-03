ed24694c073bafd0f61830b8a3ed51a83dd0bfa4
Nick <mrdoggydog37@gmail.com>
2023-03-03 15:49:18 +0300

    Updated error function in ServerT

--- Lab 2/ServerT/ServerT/SetErrorMessageText.cpp
+++ Lab 2/ServerT/ServerT/SetErrorMessageText.cpp
@@ -7,11 +7,11 @@ std::string GetErrorMessageText(int code)
 	std::string messageText;
 	switch (code) // проверка кода возврата
 	{
-	case WSAEINTR: messageText = "WSAEINTR"; break;
-	case WSAEACCES: messageText = "WSAEACCES"; break;
-		//..........коды WSAGetLastError ..........................
-	case WSASYSCALLFAILURE: messageText = "WSASYSCALLFAILURE"; break;
-	default: messageText = "***ERROR***"; break;
+	case WSAEINTR: messageText = "Error code: WSAEINTR"; break;
+	case WSAEACCES: messageText = "Error code: WSAEACCES"; break;
+	case WSASYSCALLFAILURE: messageText = "Error code: WSASYSCALLFAILURE"; break;
+	case WSAECONNREFUSED: messageText = "Connection refused! Error code: WSAECONNREFUSED"; break;
+	default: messageText = "Error! Error code: " + std::to_string(code); break;
 	};
 	return messageText;
 };
